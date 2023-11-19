c750763fc1f7ca37b6a96e87a8e2e88bb3efb3c5
solvld <sol.vld@yandex.ru>
2023-11-19 23:17:33 +0400

    add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
+import './App.scss';
 import { datesInfo } from './DatesInfo';
 import CardTable from './Components/CardTable/CardTable'
 import DateHero from './Components/DateHero/DeteHero';
+import Header from './Components/Header/Header';
 
 
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <DateHero />
-      <hr />
-      {/* <CardTable /> */}
-      <div>{datesInfo.length}</div>
     </div>
   );
 }
